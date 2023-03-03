dc0ce48de04f7ee54b119ca713853497ef132437
leverh <leverh@gmail.com>
2023-03-03 14:12:28 +0000

    Add global variables

--- run.py
+++ run.py
@@ -1,3 +1,6 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+playground = 10
+nums_ships = 5
+ships = []
+
