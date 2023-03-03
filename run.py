4e1ced9dd8eaef1d84f65034047592c05b1b89ce
leverh <leverh@gmail.com>
2023-03-03 14:20:21 +0000

    Add game grid

--- run.py
+++ run.py
@@ -4,4 +4,11 @@ playground = 10
 nums_ships = 5
 ships = []
 
-print(playground)
+grid = []
+for i in range(playground):
+    row = []
+for a in range(playground):
+    row.append('-')
+grid.append(row)
+
+print(i)
