202b95eafd2dab0ab0f6bdd66b3cee823155ff1e
leverh <leverh@gmail.com>
2023-03-03 14:25:13 +0000

    Add docstrings to code

--- run.py
+++ run.py
@@ -1,8 +1,16 @@
 import random
 
+
+"""
+Global Variables
+"""
 playground = 10
 nums_ships = 5
 ships = []
+
+"""
+Creating the grid for the game
+"""
 
 grid = []
 for i in range(playground):
@@ -11,4 +19,4 @@ for a in range(playground):
     row.append('-')
 grid.append(row)
 
-print(i)
+
