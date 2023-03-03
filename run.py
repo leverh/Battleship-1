9c32e373c2ccd6f6565400162b817485a9041a9c
leverh <leverh@gmail.com>
2023-03-03 14:35:07 +0000

    Add docstrings to game_grid and print_grid to terminal

--- run.py
+++ run.py
@@ -18,13 +18,19 @@ for a in range(playground):
     row.append('-')
 grid.append(row)
 
+"""
+Function to start the game grid
+"""
 def game_grid():
     for i in range(playground):
         for a in range(plaground):
             grid[i][a] = '_'
 
+"""
+Function to print the game grid onto terminal
+"""
 def print_grid():
     for row in grid:
         print(''.join(row))
 
-print_grid()
+
